b98995eb02ca10f2b5b3a9c18c89bc1addc217a8
anuj-badhwar <anuj.badhwar@outlook.com>
2017-10-26 17:22:08 +0530

    Morpho 17x3

--- main.py
+++ main.py
@@ -5,11 +5,21 @@ def preprocess(img):
 	cv2.imshow("s",img)
 	imgBlurred = cv2.GaussianBlur(img, (5,5), 0)
 	gray = cv2.cvtColor(imgBlurred, cv2.COLOR_BGR2GRAY)
-	sobelx = cv2.Sobel(gray,cv2.CV_8U,1,0,ksize=7)
-	#sobelx.convertTo(sobel,cv2.CV_8U);
-	#ret2,th2 = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-	cv2.imshow("e",sobelx)
+	sobelx = cv2.Sobel(gray,cv2.CV_8U,1,0,ksize=3)
+	ret2,threshold_img = cv2.threshold(sobelx,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+	cv2.imshow("e",threshold_img)
+	
+
+	element = cv2.getStructuringElement(shape=cv2.MORPH_RECT, ksize=(17, 3))
+	morph_img_threshold = threshold_img.copy()
+	cv2.morphologyEx(src=threshold_img, op=cv2.MORPH_CLOSE, kernel=element, dst=morph_img_threshold)
+	cv2.imshow("et",morph_img_threshold)
 	cv2.waitKey(0)
+
+	contours, hierarchy = cv2.findContours(morph_img_threshold,
+                                           mode=cv2.RETR_EXTERNAL,
+                                           method=cv2.CHAIN_APPROX_NONE)
+
 	
 
 if __name__ == '__main__':
