ac3a9f616617f12eef9aabd80b6bab91808cb5dc
anuj-badhwar <anuj.badhwar@outlook.com>
2017-11-01 00:46:09 +0530

    Minor Changes

--- main.py
+++ main.py
@@ -10,15 +10,12 @@ def preprocess(img):
 	imgBlurred = cv2.GaussianBlur(img, (5,5), 0)
 	gray = cv2.cvtColor(imgBlurred, cv2.COLOR_BGR2GRAY)
 
-	#gray = cv2.equalizeHist(gray)
-
 	sobelx = cv2.Sobel(gray,cv2.CV_8U,1,0,ksize=3)
 	ret2,threshold_img = cv2.threshold(sobelx,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 	return threshold_img
 
 def cleanPlate(plate):
 	print "CLEANING PLATE. . ."
-	#plate = cv2.GaussianBlur(plate, (3,3), 0)
 	gray = cv2.cvtColor(plate, cv2.COLOR_BGR2GRAY)
 	kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (1, 1))
 	thresh= cv2.dilate(gray, kernel, iterations=1)
@@ -39,7 +36,7 @@ def cleanPlate(plate):
 			return plate,None
 
 		cleaned_final = thresh[y:y+h, x:x+w]
-		cv2.imshow("Function",cleaned_final)
+		#cv2.imshow("Function Test",cleaned_final)
 		return cleaned_final,[x,y,w,h]
 
 	else:
@@ -75,7 +72,7 @@ def ratioCheck(area, width, height):
 
 def isMaxWhite(plate):
 	avg = np.mean(plate)
-	if(avg>=125):
+	if(avg>=115):
 		return True
 	else:
  		return False
@@ -102,7 +99,7 @@ def validateRotation(rect):
 
 
 
-def func(img,contours):
+def cleanAndRead(img,contours):
 	count=0
 	for i,cnt in enumerate(contours):
 		min_rect = cv2.minAreaRect(cnt)
@@ -124,27 +121,28 @@ def func(img,contours):
 					cv2.waitKey(0)
 					plate_im = Image.fromarray(clean_plate)
 					text = tess.image_to_string(plate_im, lang='eng')
-					print "test : ",text
+					print "Detected Text : ",text
 					img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-					cv2.imshow("RECTANGLES",img)
+					cv2.imshow("Detected Plate",img)
 					cv2.waitKey(0)
 
-	print "No. of final cont : " , count
+	#print "No. of final cont : " , count
 
 
 
 if __name__ == '__main__':
-	print "START"
-	#img = cv2.imread("testData/test.jpeg")
-	img = cv2.imread("testData/test.jpeg")
+	print "DETECTING PLATE . . ."
+
+	#img = cv2.imread("testData/Final.JPG")
+	img = cv2.imread("testData/test4.jpg")
 	threshold_img = preprocess(img)
 	contours= extract_contours(threshold_img)
 
-	if len(contours)!=0:
-		print len(contours) #Test
+	#if len(contours)!=0:
+		#print len(contours) #Test
 		# cv2.drawContours(img, contours, -1, (0,255,0), 1)
 		# cv2.imshow("Contours",img)
 		# cv2.waitKey(0)
 
 
-	func(img,contours)
+	cleanAndRead(img,contours)
